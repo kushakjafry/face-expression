3398e500d08c989be0886f0414accdee9f937190
Kushak Jafry <kushakjafry@gmail.com>
2022-06-14 08:43:05 +0530

    commit mocap4face in this branch

--- src/components/Demo/DemoApp.tsx
+++ src/components/Demo/DemoApp.tsx
@@ -8,8 +8,8 @@ function DemoApp() {
     <section className="py-20 mt-20" id="demo">
       {/** Uncomment <Canvas /> and comment <ThreeCanvas /> will change canvas to alter-core canvas. we can use alter's model */}
       {/* <Canvas /> */}
-      {/* <ThreeCanvas /> */}
-      <KalidokitCanvas />
+      <ThreeCanvas />
+      {/* <KalidokitCanvas /> */}
     </section>
   );
 }
